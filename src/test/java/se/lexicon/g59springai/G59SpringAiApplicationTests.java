384b0fcddd72e471bd5f754e0e3637b5af67514c
Mehrdad <mehrdad.javan@lexicon.se>
2026-05-06 09:18:53 +0200

    Initial commit

---
+++ src/test/java/se/lexicon/g59springai/G59SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g59springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G59SpringAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
